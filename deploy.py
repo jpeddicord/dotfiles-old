b85b98ac80544501cbf9d76406a046efbe7e6cd4
Jacob Peddicord <jacob@peddicord.net>
2013-07-27 23:07:26 -0700

    Time for zsh.
    Trying out prezto to start (oh-my-zsh is waaay too much) and
    some of my own customizations.

--- deploy.py
+++ deploy.py
@@ -5,6 +5,7 @@ It will mess with your own dotfiles and use mine instead.
 """
 
 import os
+from subprocess import call
 
 
 # links will be installed in the order they are here
@@ -13,6 +14,11 @@ LINKS = [
     ('vim', '.vim'),
     ('vim/vimrc', '.vimrc'),
     ('vim/gvimrc', '.gvimrc'),
+    ('zshenv', '.zshenv'),
+    ('zlogin', '.zlogin'),
+    ('zshrc', '.zshrc'),
+    ('zprezto', '.zprezto'),
+    ('zpreztorc', '.zpreztorc'),
     ('bashrc', '.bashrc'),
     ('gitconfig', '.gitconfig'),
     ('profile', '.profile'),
@@ -20,36 +26,47 @@ LINKS = [
     ('tmux.conf', '.tmux.conf'),
 ]
 
-for source, name in LINKS:
-    print "Installing", source
+def setup():
+    print "Updating submodules..."
+    call(['git', 'submodule', 'update', '--init', '--recursive'])
+    print "Submodule update complete."
 
-    # sanity check
-    if not os.path.exists(source):
-        print "  Source file not found!"
-        continue
+def make_links(links):
+    for source, name in links:
+        print "Installing", source
 
-    source = os.path.abspath(source)
-    location = os.path.join(os.path.expanduser('~'), name)
-
-    # check if a link was already made
-    if os.path.islink(location):
-
-        if os.readlink(location) == source:
-            print "  Link already present; skipping"
+        # sanity check
+        if not os.path.exists(source):
+            print "  Source file not found!"
             continue
-        else:
-            print "  Link present but pointed elsewhere; repairing"
-            os.remove(location)
 
-    # or if something else was there
-    elif os.path.exists(location):
-        print "  File exists; removing to link"
-        renamed = location + "~"
-        os.rename(location, renamed)
-        print "  Old file backed up as", renamed
+        source = os.path.abspath(source)
+        location = os.path.join(os.path.expanduser('~'), name)
 
-    print "  Linking", source, "at", location
-    try:
-        os.symlink(source, location)
-    except Exception as ex:
-        print "  Failed ({0})".format(ex)
+        # check if a link was already made
+        if os.path.islink(location):
+
+            if os.readlink(location) == source:
+                print "  Link already present; skipping"
+                continue
+            else:
+                print "  Link present but pointed elsewhere; repairing"
+                os.remove(location)
+
+        # or if something else was there
+        elif os.path.exists(location):
+            print "  File exists; removing to link"
+            renamed = location + "~"
+            os.rename(location, renamed)
+            print "  Old file backed up as", renamed
+
+        print "  Linking", source, "at", location
+        try:
+            os.symlink(source, location)
+        except Exception as ex:
+            print "  Failed ({0})".format(ex)
+
+
+if __name__ == '__main__':
+    setup()
+    make_links(LINKS)
