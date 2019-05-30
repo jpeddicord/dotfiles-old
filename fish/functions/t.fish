8a3bfc2bd9f27e3590cff8f2aca6915c8f401e33
Jacob Peddicord <jacob@peddicord.net>
2019-05-29 21:14:07 -0700

    Fix 't' function

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -2,6 +2,7 @@ function t
     if test (count $argv) -eq 0
         tmux list-sessions
     else
-        tmux attach-session -t $argv[1] or tmux new-session -s $argv[1]
+        tmux attach-session -t $argv[1]
+        or tmux new-session -s $argv[1]
     end
 end
