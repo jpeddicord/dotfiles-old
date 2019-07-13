eee277ca8abdff9672f61ade0e52b2ea035a1661
Jacob Peddicord <jacob@peddicord.net>
2019-07-13 10:58:46 -0700

    Fix fzf path

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
     set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
     set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-    source ~/.dotfiles/fzf/shell/key-bindings.fish
+    source ~/.dotfiles/modules/fzf/shell/key-bindings.fish
     fzf_key_bindings
 end
